3075f340849052a3c49cc27affd5f32d017b4885
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2023-05-25 11:34:22 +0200

    add in memory storage

--- src/adapters/storage.rs
+++ src/adapters/storage.rs
@@ -1,41 +1,114 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
 use async_trait::async_trait;
+use ethers::types::{transaction::response, H256};
 use fuels::tx::Bytes32;
 
 use crate::{common::EthTxStatus, errors::Result};
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct EthTxSubmission {
-    status: EthTxStatus,
-    tx_id: u64, //TODO: change to ETH tx id type
     fuel_block_height: u64,
-}
-
-pub struct StorageEntry {
-    id: u64,
-    value: EthTxSubmission,
+    status: EthTxStatus,
+    tx_hash: H256,
 }
 
 #[async_trait]
 pub trait Storage {
-    async fn insert(&self, submission: EthTxSubmission) -> Result<u64>;
-    async fn update(&self, entry: StorageEntry) -> Result<()>;
-    async fn last_entry(&self) -> Result<Option<StorageEntry>>;
-    async fn last_successful_tx(&self) -> Result<Option<StorageEntry>>;
+    async fn insert(&self, submission: EthTxSubmission) -> Result<()>;
+    async fn update(&self, entry: EthTxSubmission) -> Result<()>;
+    async fn submission_w_latest_block(&self) -> Result<Option<EthTxSubmission>>;
 }
 
-pub struct FakeStorage {}
+pub struct InMemoryStorage {
+    pub storage: Arc<Mutex<HashMap<u64, EthTxSubmission>>>,
+}
+
+impl InMemoryStorage {
+    pub fn new() -> Self {
+        Self {
+            storage: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
 
 #[async_trait]
-impl Storage for FakeStorage {
-    async fn insert(&self, submission: EthTxSubmission) -> Result<u64> {
-        todo!()
+impl Storage for InMemoryStorage {
+    async fn insert(&self, submission: EthTxSubmission) -> Result<()> {
+        self.storage
+            .lock()
+            .unwrap()
+            .insert(submission.fuel_block_height, submission);
+        Ok(())
     }
-    async fn update(&self, entry: StorageEntry) -> Result<()> {
-        todo!()
+
+    async fn update(&self, entry: EthTxSubmission) -> Result<()> {
+        self.storage
+            .lock()
+            .unwrap()
+            .insert(entry.fuel_block_height, entry);
+        Ok(())
     }
-    async fn last_entry(&self) -> Result<Option<StorageEntry>> {
-        todo!()
+
+    async fn submission_w_latest_block(&self) -> Result<Option<EthTxSubmission>> {
+        let res = self
+            .storage
+            .lock()
+            .unwrap()
+            .iter()
+            .max_by_key(|(k, v)| k.clone())
+            .map(|(_, v)| v.clone())
+            .clone();
+        Ok(res)
     }
-    async fn last_successful_tx(&self) -> Result<Option<StorageEntry>> {
-        todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn can_insert_and_find_latest_block() {
+        let storage = InMemoryStorage::new();
+
+        let latest_block = EthTxSubmission {
+            fuel_block_height: 10,
+            status: EthTxStatus::Pending,
+            tx_hash: H256::default(),
+        };
+        storage.insert(latest_block.clone()).await.unwrap();
+        storage
+            .insert(EthTxSubmission {
+                fuel_block_height: 9,
+                status: EthTxStatus::Pending,
+                tx_hash: H256::default(),
+            })
+            .await
+            .unwrap();
+
+        let actual = storage.submission_w_latest_block().await.unwrap().unwrap();
+
+        assert_eq!(actual, latest_block);
+    }
+
+    #[tokio::test]
+    async fn can_update_block() {
+        let storage = InMemoryStorage::new();
+
+        let mut latest_block = EthTxSubmission {
+            fuel_block_height: 10,
+            status: EthTxStatus::Pending,
+            tx_hash: H256::default(),
+        };
+        storage.insert(latest_block.clone()).await.unwrap();
+        latest_block.status = EthTxStatus::Commited;
+        storage.update(latest_block.clone()).await.unwrap();
+
+        let actual = storage.submission_w_latest_block().await.unwrap().unwrap();
+
+        assert_eq!(actual, latest_block);
     }
 }
--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum EthTxStatus {
     Pending,
     Commited,
