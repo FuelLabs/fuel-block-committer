9610c8423f4b460b905c3dd057fdc4ea9722c042
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2023-05-25 16:44:57 +0200

    rewrite tests to be simpler, leave only happy path for the controller

--- src/services/block_watcher.rs
+++ src/services/block_watcher.rs
@@ -36,7 +36,11 @@ impl BlockWatcher {
 
         let latest_block_submission = self.storage.submission_w_latest_block().await?;
 
-        if self.should_propagate_update(&current_block, latest_block_submission) {
+        if Self::should_propagate_update(
+            self.commit_epoch,
+            &current_block,
+            latest_block_submission.as_ref(),
+        ) {
             self.tx_fuel_block.send(current_block).await?; // todo: handle error
         }
 
@@ -44,9 +48,9 @@ impl BlockWatcher {
     }
 
     fn should_propagate_update(
-        &self,
+        commit_epoch: u32,
         current_block: &FuelBlock,
-        last_block_submission: Option<EthTxSubmission>,
+        last_block_submission: Option<&EthTxSubmission>,
     ) -> bool {
         let Some(submission) = last_block_submission else {
             return true;
@@ -59,7 +63,7 @@ impl BlockWatcher {
         let height_diff = current_block.header.height - submission.fuel_block_height;
         match submission.status {
             EthTxStatus::Pending => false,
-            EthTxStatus::Commited if height_diff % self.commit_epoch != 0 => false,
+            EthTxStatus::Commited if height_diff % commit_epoch != 0 => false,
             _ => true,
         }
     }
@@ -71,6 +75,7 @@ mod tests {
 
     use ethers::types::H256;
     use fuels::{tx::Bytes32, types::block::Header as FuelBlockHeader};
+    use mockall::predicate::ge;
 
     use super::*;
     use crate::{
@@ -86,12 +91,20 @@ mod tests {
         // given
         let (tx, mut rx) = tokio::sync::mpsc::channel(10);
 
-        let block = given_a_block(0);
+        let block = given_a_block(5);
 
         let block_fetcher = given_fetcher_that_returns(vec![block.clone()]);
 
         let storage = InMemoryStorage::new();
-        let block_watcher = BlockWatcher::new(1, tx, block_fetcher, storage);
+        storage
+            .insert(EthTxSubmission {
+                fuel_block_height: 3,
+                status: EthTxStatus::Commited,
+                tx_hash: H256::default(),
+            })
+            .await
+            .unwrap();
+        let block_watcher = BlockWatcher::new(2, tx, block_fetcher, storage);
 
         // when
         block_watcher.run().await.unwrap();
@@ -106,90 +119,57 @@ mod tests {
 
     #[tokio::test]
     async fn will_not_propagate_if_last_tx_is_pending() {
-        // given
-        let (tx, mut rx) = tokio::sync::mpsc::channel(10);
+        let current_block = given_a_block(2);
+        let last_block_submission = given_pending_submission(1);
 
-        let block = given_a_block(2);
+        let should_propagate =
+            BlockWatcher::should_propagate_update(1, &current_block, Some(&last_block_submission));
 
-        let storage = InMemoryStorage::new();
-        storage.insert(given_pending_submission(1)).await.unwrap();
-
-        let block_fetcher = given_fetcher_that_returns(vec![block.clone()]);
-
-        let block_watcher = BlockWatcher::new(1, tx, block_fetcher, storage);
-
-        // when
-        block_watcher.run().await.unwrap();
-
-        //then
-        let err = rx.try_recv().expect_err("Should have failed");
-        assert!(matches!(err, tokio::sync::mpsc::error::TryRecvError::Empty))
+        assert!(!should_propagate);
     }
 
     #[tokio::test]
     async fn respects_epoch_when_posting_block_updates() {
-        // given
-        let (tx, mut rx) = tokio::sync::mpsc::channel(10);
+        let commit_epoch = 3;
 
-        let blocks = (1..=3)
-            .map(|height| given_a_block(height))
-            .collect::<Vec<_>>();
+        let last_block_submission = EthTxSubmission {
+            fuel_block_height: 1,
+            status: EthTxStatus::Commited,
+            tx_hash: Default::default(),
+        };
 
-        let block_fetcher = given_fetcher_that_returns(blocks.clone());
+        let check_should_submit = |block_height, should_submit| {
+            let current_block = given_a_block(block_height);
+            let actual = BlockWatcher::should_propagate_update(
+                commit_epoch,
+                &current_block,
+                Some(&last_block_submission),
+            );
 
-        let epoch = 3;
-        let storage = InMemoryStorage::new();
-        storage
-            .insert(EthTxSubmission {
-                fuel_block_height: 0,
-                status: EthTxStatus::Commited,
-                tx_hash: Default::default(),
-            })
-            .await
-            .unwrap();
-        let block_watcher = BlockWatcher::new(epoch, tx, block_fetcher, storage);
-        block_watcher.run().await.unwrap();
-        block_watcher.run().await.unwrap();
+            assert_eq!(actual, should_submit);
+        };
 
-        // when
-        block_watcher.run().await.unwrap();
-
-        //then
-        assert_eq!(rx.try_recv().ok().unwrap(), *blocks.last().unwrap());
-
-        let err = rx.try_recv().expect_err("Should have been empty");
-        assert!(matches!(err, tokio::sync::mpsc::error::TryRecvError::Empty))
+        check_should_submit(2, false);
+        check_should_submit(3, false);
+        check_should_submit(4, true);
     }
 
     #[tokio::test]
     async fn will_post_the_next_block_after_failure() {
         // given
-        let (tx, mut rx) = tokio::sync::mpsc::channel(10);
-
-        let storage = InMemoryStorage::new();
-        storage
-            .insert(EthTxSubmission {
-                fuel_block_height: 2,
-                status: EthTxStatus::Aborted,
-                tx_hash: H256::default(),
-            })
-            .await
-            .unwrap();
-
-        let new_block = given_a_block(3);
-        let block_fetcher = given_fetcher_that_returns(vec![new_block.clone()]);
-
-        let epoch = 2;
-        let block_watcher = BlockWatcher::new(epoch, tx, block_fetcher, storage);
+        let last_block_submission = EthTxSubmission {
+            fuel_block_height: 2,
+            status: EthTxStatus::Aborted,
+            tx_hash: H256::default(),
+        };
+        let current_block = given_a_block(4);
 
         // when
-        block_watcher.run().await.unwrap();
+        let should_propagate =
+            BlockWatcher::should_propagate_update(7, &current_block, Some(&last_block_submission));
 
-        //then
-        assert_eq!(rx.try_recv().ok().unwrap(), new_block);
-
-        let err = rx.try_recv().expect_err("Should have been empty");
-        assert!(matches!(err, tokio::sync::mpsc::error::TryRecvError::Empty))
+        // then
+        assert!(should_propagate);
     }
 
     fn given_fetcher_that_returns(blocks: Vec<FuelBlock>) -> MockBlockFetcher {
