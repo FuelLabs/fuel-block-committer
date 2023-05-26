05d36144125c4a889183a7fd1532aa289edf78a5
Emir Salkic <emirsalkicart@gmail.com>
2023-05-26 15:38:26 +0200

    remove println()

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,10 +1,5 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let _config = fuel_block_committer_cli::parse();
-    println!("{:?}", _config.ethereum_wallet_key);
-    println!("{:?}", _config.ethereum_rpc);
-    println!("{:?}", _config.fuel_graphql_endpoint);
-    println!("{:?}", _config.state_contract_address);
-    println!("{:?}", _config.commit_interval);
     Ok(())
 }
