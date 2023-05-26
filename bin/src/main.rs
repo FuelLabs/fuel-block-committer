9377ac3bb8f642bea6ef81844e1f0937294ceced
Emir Salkic <emirsalkicart@gmail.com>
2023-05-26 15:37:22 +0200

    add env feature

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -2,8 +2,9 @@
 async fn main() -> anyhow::Result<()> {
     let _config = fuel_block_committer_cli::parse();
     println!("{:?}", _config.ethereum_wallet_key);
-    println!("{:?}", _config.ethereum_wallet_key);
-    println!("{:?}", _config.ethereum_wallet_key);
-    println!("{:?}", _config.ethereum_wallet_key);
+    println!("{:?}", _config.ethereum_rpc);
+    println!("{:?}", _config.fuel_graphql_endpoint);
+    println!("{:?}", _config.state_contract_address);
+    println!("{:?}", _config.commit_interval);
     Ok(())
 }
--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,7 +1,5 @@
-use clap::error::ErrorKind;
-use clap::{command, Command, Parser};
+use clap::{command, Parser};
 use fuel_types::{Bytes20, Bytes32};
-use std::str::FromStr;
 use url::Url;
 
 const ETHEREUM_RPC: &str = "https://mainnet.infura.io/v3/YOUR_PROJECT_ID";
@@ -23,72 +21,53 @@ pub struct Config {
     name = "fuel-block-committer",
     version,
     about,
-    propagate_version = true,
+    propagate_version = true
 )]
 struct Cli {
     /// Ethereum wallet key
     #[arg(
         long,
+        env = "ETHEREUM_WALLET_KEY",
         value_name = "BYTES32",
         help = "The secret key authorized by the L1 bridging contracts to post block commitments.",
         required = false
     )]
-    ethereum_wallet_key: Option<Bytes32>,
+    ethereum_wallet_key: Bytes32,
 
     /// Ethereum RPC
-    #[arg(long, default_value = ETHEREUM_RPC, value_name = "URL", help = "URL to a Ethereum RPC endpoint.")]
+    #[arg(long,
+    env = "ETHEREUM_RPC",
+    default_value = ETHEREUM_RPC, value_name = "URL", help = "URL to a Ethereum RPC endpoint.")]
     ethereum_rpc: Url,
 
     /// Fuel GraphQL endpoint
-    #[arg(long, default_value = FUEL_GRAPHQL_ENDPOINT, value_name = "URL", help = "URL to a fuel-core graphql endpoint.")]
+    #[arg(long,
+    env = "FUEL_GRAPHQL_ENDPOINT",
+    default_value = FUEL_GRAPHQL_ENDPOINT, value_name = "URL", help = "URL to a fuel-core graphql endpoint.")]
     fuel_graphql_endpoint: Url,
 
     /// State contract address
-    #[arg(long, default_value_t = STATE_CONTRACT_ADDRESS, value_name = "BYTES20", help = "Ethereum address of the fuel chain state contract.")]
+    #[arg(long,
+    env = "STATE_CONTRACT_ADDRESS",
+    default_value_t = STATE_CONTRACT_ADDRESS, value_name = "BYTES20", help = "Ethereum address of the fuel chain state contract.")]
     state_contract_address: Bytes20,
 
     /// Commit interval
-    #[arg(long, default_value_t = COMMIT_INTERVAL, value_name = "U32", help = "The number of fuel blocks between ethereum commits. If set to 1, then every block should be pushed to Ethereum.")]
+    #[arg(long,
+    env = "COMMIT_INTERVAL",
+    default_value_t = COMMIT_INTERVAL, value_name = "U32", help = "The number of fuel blocks between ethereum commits. If set to 1, then every block should be pushed to Ethereum.")]
     commit_interval: u32,
 }
 
 pub fn parse() -> Config {
     let cli = Cli::parse();
 
-    let ethereum_wallet_key = cli.ethereum_wallet_key.or_else(|| {
-        std::env::var("ETHEREUM_WALLET_KEY")
-            .ok()
-            .and_then(|value| Bytes32::from_str(&value).ok())
-    });
-
-    if ethereum_wallet_key.is_none() {
-        Command::error(
-            &mut Command::new("fuel-block-committer-bin --ethereum-wallet-key <BYTES32>"),
-            ErrorKind::MissingRequiredArgument,
-            "Please provide the Ethereum wallet key using the \x1B[33m'--ethereum-wallet-key'\x1B[0m command-line argument or set the \x1B[33m'ETHEREUM_WALLET_KEY'\x1B[0m environmental variable.\n"
-        ).exit();
-    }
-
     Config {
-        ethereum_wallet_key: ethereum_wallet_key.unwrap(),
-        ethereum_rpc: Url::parse(
-            &std::env::var("ETHEREUM_RPC").unwrap_or(cli.ethereum_rpc.to_string()),
-        )
-        .unwrap(),
-        fuel_graphql_endpoint: Url::parse(
-            &std::env::var("FUEL_GRAPHQL_ENDPOINT")
-                .unwrap_or(cli.fuel_graphql_endpoint.to_string()),
-        )
-        .unwrap(),
-        state_contract_address: Bytes20::from_str(
-            &std::env::var("STATE_CONTRACT_ADDRESS")
-                .unwrap_or(cli.state_contract_address.to_string()),
-        )
-        .unwrap(),
-        commit_interval: std::env::var("COMMIT_INTERVAL")
-            .unwrap_or(cli.commit_interval.to_string())
-            .parse()
-            .unwrap(),
+        ethereum_wallet_key: cli.ethereum_wallet_key,
+        ethereum_rpc: cli.ethereum_rpc,
+        fuel_graphql_endpoint: cli.fuel_graphql_endpoint,
+        state_contract_address: cli.state_contract_address,
+        commit_interval: cli.commit_interval,
     }
 }
 
