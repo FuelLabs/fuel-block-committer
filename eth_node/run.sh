d36bb6cc619a7652eecb06c818b919331ca69348
Ahmed Sagdati <37515857+segfault-magnet@users.noreply.github.com>
2024-04-02 23:28:08 +0200

    chore: update e2e tests to use the contracts from the `fuel-bridge` repo (#54)

--- eth_node/run.sh
+++ eth_node/run.sh
@@ -1,29 +1,111 @@
-#!/bin/bash
+#!/bin/sh
+set -e
+PORT="9545"
+IP="0.0.0.0"
 
-for ENV_VAR in WALLET_KEY URL; do
-    if [[ -z "${!ENV_VAR}" ]]; then
-        echo "${ENV_VAR} not set!"
-        exit 1
-    fi
-    sed "s#$ENV_VAR#${!ENV_VAR}#g" -i hardhat.config.ts;
+# Functions
+
+replace_placeholders() {
+	KEY="$1"
+	URL="$2"
+	if [[ -z "$KEY" ]] || [[ -z "$URL" ]]; then
+		echo "usage: replace_placeholders <key> <url>"
+		exit 1
+	fi
+
+	sed -i "s/KEY/$KEY/g;s#URL#$URL#g" ./hardhat.config.ts
+}
+
+spawn_eth_node() {
+	IP="$1"
+	PORT="$2"
+	if [[ -z "$IP" ]] || [[ -z "$PORT" ]]; then
+		echo "usage: spawn_eth_node <ip> <port>"
+		exit 1
+	fi
+
+	pnpm hardhat node --network hardhat --port "$PORT" --hostname "$IP" &
+	trap "kill $! 2>/dev/null" EXIT
+}
+
+wait_node_healthy() {
+	URL="$1"
+	if [[ -z "$URL" ]]; then
+		echo "usage: wait_node_healthy <url>"
+		exit 1
+	fi
+
+	curl \
+		--fail \
+		--silent \
+		-H "Content-Type: application/json" \
+		--retry-connrefused \
+		--retry 120 \
+		--retry-delay 1 \
+		-d '{"jsonrpc":"2.0","id":0,"method":"net_version","params":[]}' \
+		"$URL" >/dev/null
+}
+
+deploy_contracts() {
+	KEY="$1"
+	URL="$2"
+	if [[ -z "$KEY" ]] || [[ -z "$URL" ]]; then
+		echo "usage: deploy_contracts <key> <url>"
+		exit 1
+	fi
+	LOCALHOST_HTTP="$URL" DEPLOYER_KEY="$KEY" pnpm run node-deploy
+}
+
+usage() {
+	echo "Usage: $0 --key KEY [options]"
+	echo "  --key KEY    The private key to use for deployment"
+	echo "  -p, --port   Set the port number (default: ${PORT})"
+	echo "  -i, --ip     Set the IP address (default: ${IP})"
+	echo "  -h, --help   Display this help message"
+}
+
+# Main
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	--key)
+		KEY="$2"
+		shift
+		;;
+	-p | --port)
+		PORT="$2"
+		shift
+		;;
+	-i | --ip)
+		IP="$2"
+		shift
+		;;
+	-h | --help)
+		usage
+		exit 0
+		;;
+	*)
+		echo "Unknown parameter passed: $1"
+		usage
+		exit 1
+		;;
+	esac
+	shift
 done
 
-(npm run node 2>&1 | tee node_err)& 
-# propagate SIGTERM to the node running in the background
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+if [[ -z "$KEY" ]]; then
+	echo "Error: KEY is required."
+	usage
+	exit 1
+fi
 
-while true; do
-    if grep -q -F 'Account #0: 0xBCcA9EcB933Db2481111102E73c61C7c7C4e2366 (10000 ETH)' node_err &> /dev/null ; then
-        break
-    fi
+URL="http://$IP:$PORT"
+replace_placeholders "$KEY" "$URL"
+spawn_eth_node "$IP" "$PORT"
 
-    sleep 1;
-done
+wait_node_healthy "$URL"
 
-yes Y | npm run script-deploy
-
-# part of health checking, so that we don't start the committer before the
-# deployment is done
+deploy_contracts "$KEY" "$URL"
 touch /contracts_deployed
 
 wait
