3a6e15aa9663d6006119623c404eb44359e079f3
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2024-09-26 13:42:08 +0200

    separate e2e tests and run one by one in CI

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,8 @@ script_location="$(readlink -f "$(dirname "$0")")"
 
 workspace_cargo_manifest="$script_location/Cargo.toml"
 
+cargo test --manifest-path "$workspace_cargo_manifest" --workspace --exclude e2e
+
 # So that we may have a binary in `target/debug`
 cargo build --manifest-path "$workspace_cargo_manifest" --bin fuel-block-committer
-
-PATH="$script_location/target/debug:$PATH" cargo test --manifest-path "$workspace_cargo_manifest" --workspace
+PATH="$script_location/target/debug:$PATH" cargo test --manifest-path "$workspace_cargo_manifest" --package e2e --jobs 1
