5e0a11a065acb4c66e5fd829452ed4bc76e3c382
MujkicA <ahmedmujkic2@gmail.com>
2024-10-06 02:50:09 +0200

    update migration

--- packages/storage/migrations/0004_tx_state_added.up.sql
+++ packages/storage/migrations/0004_tx_state_added.up.sql
@@ -1,8 +1,8 @@
-ALTER TABLE l1_transaction
-  DROP CONSTRAINT l1_transaction_check;
+ALTER TABLE l1_blob_transaction
+  DROP CONSTRAINT l1_transactions_state_check;
 
-ALTER TABLE l1_transaction
-  ADD CONSTRAINT l1_transaction_state_check 
+ALTER TABLE l1_blob_transaction
+  ADD CONSTRAINT l1_blob_transaction_state_check 
   CHECK (
     state IN (0, 1, 2, 3) 
     AND (state != 1 OR finalized_at IS NOT NULL)
