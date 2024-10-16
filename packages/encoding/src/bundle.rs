692b4164338875e410274b236156975f6c58a2f9
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2024-10-16 18:58:00 +0200

    improve debug output of bundle

--- packages/encoding/src/bundle.rs
+++ packages/encoding/src/bundle.rs
@@ -3,9 +3,19 @@ mod encoder;
 pub use decoder::*;
 pub use encoder::*;
 
-#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 pub struct BundleV1 {
     pub blocks: Vec<Vec<u8>>,
+}
+
+impl std::fmt::Debug for BundleV1 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let hex_encoded = self.blocks.iter().map(hex::encode).collect::<Vec<String>>();
+
+        f.debug_struct("BundleV1")
+            .field("blocks", &hex_encoded)
+            .finish()
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
