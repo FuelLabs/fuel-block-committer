a72b0cb260456ad4f80ea1ee6b7f73816b6ec38a
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2024-10-20 00:43:08 +0200

    add more checks for decoder

--- packages/encoding/src/blob/decoder.rs
+++ packages/encoding/src/blob/decoder.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{collections::HashSet, marker::PhantomData};
 
 use anyhow::bail;
 use bitvec::{order::Msb0, slice::BitSlice, vec::BitVec};
@@ -15,6 +15,8 @@ impl Decoder {
         if blobs.is_empty() {
             bail!("No blobs to decode");
         }
+
+        // TODO: check the num bits being out of bounds of a blob size
 
         let mut indexed_data = blobs
             .iter()
@@ -39,6 +41,7 @@ impl Decoder {
 
         // Sort the data by their indices to maintain order
         indexed_data.sort_by_key(|(header, _)| header.idx);
+
         // TODO: segfault add check for when a blob has a higher idx than the ending blob
 
         // make sure all blobs have the same bundle id
@@ -53,10 +56,57 @@ impl Decoder {
             Ok(())
         })?;
 
+        let blobs_marked_as_last: Vec<_> = indexed_data
+            .iter()
+            .filter(|(header, _)| header.is_last)
+            .collect();
+
+        if blobs_marked_as_last.is_empty() {
+            bail!("no blob is marked as last");
+        }
+
+        let highest_idx = indexed_data.last().expect("At least one blob").0.idx;
+        if blobs_marked_as_last.len() > 1 {
+            let msg = blobs_marked_as_last
+                .iter()
+                .map(|(header, _)| header.idx.to_string())
+                .collect::<Vec<_>>()
+                .join(", ");
+            bail!("multiple blobs marked as being the last blob. blobs with indexes: {msg}");
+        }
+
+        if blobs_marked_as_last[0].0.idx != highest_idx {
+            bail!(
+                "blob with highest index is {}, but the blob marked as last has index {}",
+                highest_idx,
+                blobs_marked_as_last[0].0.idx
+            );
+        }
+
+        let present_idxs: HashSet<u32> =
+            indexed_data.iter().map(|(header, _)| header.idx).collect();
+
+        let missing_idxs: Vec<u32> = (0..=highest_idx)
+            .filter(|idx| !present_idxs.contains(idx))
+            .collect();
+
+        if !missing_idxs.is_empty() {
+            let msg = missing_idxs
+                .iter()
+                .map(|idx| idx.to_string())
+                .collect::<Vec<_>>()
+                .join(", ");
+            bail!("missing blobs with indexes: {msg}");
+        }
+
         // Ensure that all indices are consecutive and starting from zero
         for (expected_idx, (header, _)) in indexed_data.iter().enumerate() {
             if expected_idx as u32 != header.idx {
-                bail!("Expected index {}, got {}", expected_idx, header.idx);
+                bail!(
+                    "unexpected blob idx of {}, expected the idx of {}",
+                    header.idx,
+                    expected_idx
+                );
             }
         }
 
@@ -93,5 +143,166 @@ impl Decoder {
         let (header, _) = Header::decode(buffer)?;
 
         Ok(header)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::blob;
+
+    #[test]
+    fn complains_if_no_blobs_are_given() {
+        // given
+        let decoder = super::Decoder::default();
+
+        // when
+        let err = decoder.decode(&[]).unwrap_err();
+
+        // then
+        assert_eq!(err.to_string(), "No blobs to decode");
+    }
+
+    #[test]
+    fn complains_if_there_is_a_blob_from_a_different_bundle() {
+        // given
+        let bundle_1_blobs = blob::Encoder::default().encode(&[100], 0).unwrap();
+        let bundle_2_blobs = blob::Encoder::default().encode(&[100], 1).unwrap();
+
+        let all_blobs = [bundle_1_blobs, bundle_2_blobs].concat();
+
+        // when
+        let err = super::Decoder::default().decode(&all_blobs).unwrap_err();
+
+        // then
+        assert_eq!(
+            err.to_string(),
+            "All blobs must have the same bundle id, got 1 and 0"
+        );
+    }
+
+    #[test]
+    fn complains_if_there_are_duplicate_idx() {
+        // given
+        let bundle_1_blobs = blob::Encoder::default().encode(&[0; 200_000], 0).unwrap();
+        assert_eq!(bundle_1_blobs.len(), 2);
+
+        let with_duplication = [
+            bundle_1_blobs[0].clone(),
+            bundle_1_blobs[0].clone(),
+            bundle_1_blobs[1].clone(),
+        ];
+
+        // when
+        let err = super::Decoder::default()
+            .decode(&with_duplication)
+            .unwrap_err();
+
+        // then
+        assert_eq!(
+            err.to_string(),
+            "unexpected blob idx of 0, expected the idx of 1"
+        );
+    }
+
+    #[test]
+    fn complains_if_index_missing() {
+        // given
+        let blobs = blob::Encoder::default()
+            .encode(&vec![0; 400_000], 0)
+            .unwrap();
+        assert_eq!(blobs.len(), 4);
+
+        let blobs_with_holes = [blobs[0].clone(), blobs[3].clone()];
+
+        // when
+        let err = super::Decoder::default()
+            .decode(&blobs_with_holes)
+            .unwrap_err();
+
+        // then
+        assert_eq!(err.to_string(), "missing blobs with indexes: 1, 2");
+    }
+
+    #[test]
+    fn complains_if_no_blob_is_marked_as_last() {
+        // given
+        let blobs = blob::Encoder::default()
+            .encode(&vec![0; 400_000], 0)
+            .unwrap();
+        assert_eq!(blobs.len(), 4);
+
+        let leave_out_the_last_blob = &blobs[..3];
+
+        // when
+        let err = super::Decoder::default()
+            .decode(leave_out_the_last_blob)
+            .unwrap_err();
+
+        // then
+        assert_eq!(err.to_string(), "no blob is marked as last");
+    }
+
+    #[test]
+    fn multiple_blobs_marked_as_being_the_last() {
+        // given
+        let four_blob_bundle = blob::Encoder::default()
+            .encode(&vec![0; 400_000], 0)
+            .unwrap();
+        assert_eq!(four_blob_bundle.len(), 4);
+
+        let three_blob_bundle = blob::Encoder::default()
+            .encode(&vec![0; 300_000], 0)
+            .unwrap();
+        assert_eq!(three_blob_bundle.len(), 3);
+
+        let blobs_with_multiple_last = [
+            four_blob_bundle[0].clone(),
+            four_blob_bundle[1].clone(),
+            three_blob_bundle[2].clone(),
+            four_blob_bundle[3].clone(),
+        ];
+
+        // when
+        let err = super::Decoder::default()
+            .decode(&blobs_with_multiple_last)
+            .unwrap_err();
+
+        // then
+        assert_eq!(
+            err.to_string(),
+            "multiple blobs marked as being the last blob. blobs with indexes: 2, 3"
+        );
+    }
+
+    #[test]
+    fn complains_if_the_last_blob_doesnt_have_the_highest_idx() {
+        // given
+        let four_blob_bundle = blob::Encoder::default()
+            .encode(&vec![0; 400_000], 0)
+            .unwrap();
+        assert_eq!(four_blob_bundle.len(), 4);
+
+        let two_blob_bundle = blob::Encoder::default()
+            .encode(&vec![0; 200_000], 0)
+            .unwrap();
+        assert_eq!(two_blob_bundle.len(), 2);
+
+        let blobs_with_last_not_highest_idx = [
+            four_blob_bundle[0].clone(),
+            four_blob_bundle[1].clone(),
+            four_blob_bundle[2].clone(),
+            two_blob_bundle[1].clone(),
+        ];
+
+        // when
+        let err = super::Decoder::default()
+            .decode(&blobs_with_last_not_highest_idx)
+            .unwrap_err();
+
+        // then
+        assert_eq!(
+            err.to_string(),
+            "blob with highest index is 2, but the blob marked as last has index 1"
+        );
     }
 }
