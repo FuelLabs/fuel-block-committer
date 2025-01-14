53eb116bb1ac3d652ff0eb73bbbc69f619d6fa0f
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2025-01-14 12:43:49 +0100

    duplicate test

--- packages/services/src/state_committer/fee_algo.rs
+++ packages/services/src/state_committer/fee_algo.rs
@@ -448,24 +448,6 @@ mod tests {
     )]
         #[test_case(
         Setup {
-            old_fees: Fees { base_fee_per_gas: 4000, reward: 8000, base_fee_per_blob_gas: 4000},
-            new_fees: Fees { base_fee_per_gas: 3000, reward: 7000, base_fee_per_blob_gas: 3500},
-            num_blobs: 6,
-            num_l2_blocks_behind: 0,
-            should_send: true,
-        },
-        Config {
-            sma_periods: SmaPeriods { short: 2.try_into().unwrap(), long: 6.try_into().unwrap()},
-            fee_thresholds: FeeThresholds {
-                max_l2_blocks_behind: 100.try_into().unwrap(),
-                always_acceptable_fee: 0,
-                ..Default::default()
-            }
-        };
-        "Should send because multiple short-term fees are lower"
-    )]
-        #[test_case(
-        Setup {
             old_fees: Fees { base_fee_per_gas: 5000, reward: 5000, base_fee_per_blob_gas: 5000},
             new_fees: Fees { base_fee_per_gas: 5000, reward: 5000, base_fee_per_blob_gas: 5000},
             num_blobs: 6,
