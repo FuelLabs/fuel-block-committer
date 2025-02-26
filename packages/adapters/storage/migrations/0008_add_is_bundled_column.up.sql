d0e91cc59106378dabc387784ef1240c513cf3a2
segfault-magnet <ahmed.sagdati.ets@gmail.com>
2025-02-26 16:45:50 +0100

    comment

--- packages/adapters/storage/migrations/0008_add_is_bundled_column.up.sql
+++ packages/adapters/storage/migrations/0008_add_is_bundled_column.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 ALTER TABLE fuel_blocks 
   ADD COLUMN IF NOT EXISTS is_bundled BOOLEAN NOT NULL DEFAULT false;
 
--- Create an index to help queries filter quickly by is_bundled and height.
+-- to help queries filter quickly by is_bundled and height.
 CREATE INDEX IF NOT EXISTS idx_fuel_blocks_is_bundled_height 
   ON fuel_blocks(is_bundled, height);
 
