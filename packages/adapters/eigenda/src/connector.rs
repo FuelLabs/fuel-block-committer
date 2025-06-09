999d7d6f852d75623a8e022e6c4759427a18a0c2
Hannes Karppila <hannes.karppila@gmail.com>
2025-06-09 10:13:40 +0300

    clippy

--- packages/adapters/eigenda/src/connector.rs
+++ packages/adapters/eigenda/src/connector.rs
@@ -155,7 +155,7 @@ where
         // This could be changed to a configurable parameter
         let eth_rpc_str = "https://ethereum-holesky-rpc.publicnode.com";
         let eth_rpc_url =
-            SecretUrl::new(Url::parse(eth_rpc_str).map_err(|e| Error::InvalidRPCUrl(e))?);
+            SecretUrl::new(Url::parse(eth_rpc_str).map_err(Error::InvalidRPCUrl)?);
 
         // TODO: make configurable
         const CERT_VERIFIER_ADDRESS: &str = "fe52fe1940858dcb6e12153e2104ad0fdfbe1162"; // holesky cert verifier address
@@ -172,7 +172,7 @@ where
         // Create PayloadDisperser instance
         let eigen_client = EigenClient::new(config, signer)
             .await
-            .map_err(|e| Error::EigenDAClientInit(e))?;
+            .map_err(Error::EigenDAClientInit)?;
 
         let throughput_quota =
             Quota::per_second(throughput.bytes_per_sec).allow_burst(throughput.max_burst);
@@ -198,13 +198,13 @@ where
             .payload_disperser
             .send_payload(Payload::new(padded_data))
             .await
-            .map_err(|e| Error::BlockDispatchFailed(e))?;
+            .map_err(Error::BlockDispatchFailed)?;
 
         Ok(blob_id.to_hex())
     }
 
     pub async fn check_blob_status(&self, blob_id: &str) -> Result<DispersalStatus> {
-        let blob_key = BlobKey::from_hex(blob_id).map_err(|e| Error::InvalidBlobKey(e))?;
+        let blob_key = BlobKey::from_hex(blob_id).map_err(Error::InvalidBlobKey)?;
 
         let response = match self
             .eigen_client
