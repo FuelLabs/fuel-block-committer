3d1bee03632149adfe1ba044ac39c66b2559c7d7
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-06-20 22:09:33 +0530

    dev: fix clippy issue

--- e2e/tests/tests/eigen_state.rs
+++ e2e/tests/tests/eigen_state.rs
@@ -100,7 +100,8 @@ fn extract_metric_value(input: &str, target_metric: &str) -> Option<u64> {
         // Check for target metric at start of line
         if line.starts_with(target_metric) {
             // Value is always the second component
-            if let Some(value) = line.split_whitespace().nth(1)?.parse().ok() {
+            if let Ok(value) = line.split_whitespace().nth(1)?.parse()
+            {
                 return Some(value);
             }
         }
