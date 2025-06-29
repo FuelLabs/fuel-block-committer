cc0572ecd2208a38df08d328c434f3eb92e887e2
Mitch Turner <james.mitchell.turner@gmail.com>
2025-06-29 15:38:55 -0600

    Update `alloy` to newest version

--- e2e/build.rs
+++ e2e/build.rs
@@ -244,7 +244,6 @@ mod foundry {
         let output = tokio::process::Command::new("forge")
             .arg("init")
             .arg("--no-git")
-            .arg("--no-commit")
             .stdin(std::process::Stdio::null())
             .current_dir(dir)
             .kill_on_drop(true)
